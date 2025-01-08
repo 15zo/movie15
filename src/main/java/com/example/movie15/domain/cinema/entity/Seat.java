dd38867429ff7bf7df269e533bc1ee3618176bb2
diqkraud <daesan12>
2025-01-08 13:55:25 +0900

    Fix : Entity수정

--- src/main/java/com/example/movie15/domain/cinema/entity/Seat.java
+++ src/main/java/com/example/movie15/domain/cinema/entity/Seat.java
@@ -15,11 +15,11 @@ public class Seat {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 좌석 고유 식별자
+    private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "hall_id", nullable = false)
-    private Hall hall; // 상영관 식별자
+    @JoinColumn(name = "hall_id", nullable = false) // 상영관과 다대일 관계
+    private Hall hall;
 
     @Column(nullable = false)
     private Integer rowNum; // 행 번호
@@ -29,6 +29,9 @@ public class Seat {
 
     @Column(nullable = false, length = 20)
     private String type; // 좌석 타입 (예: 일반석, VIP석)
+
+    @Column(nullable = false)
+    private Boolean status; // 예약 가능 여부
 
     @OneToMany(mappedBy = "seat", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BookingSeat> bookingSeatList = new ArrayList<>();
