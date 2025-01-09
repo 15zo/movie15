285a6db1ff930bf54192f4e8e75a7c3be2d23cc0
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-09 13:56:51 +0900

    Feat: 영화ID로 해당영화에 대한 모든리뷰조회기능 추가

--- src/main/java/com/example/movie15/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/movie15/domain/review/repository/ReviewRepository.java
@@ -11,4 +11,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     // 특정 사용자 ID에 해당하는 리뷰 목록 조회. N+1 방지 (JOIN FETCH)
     @Query("SELECT r FROM Review r JOIN FETCH r.movie WHERE r.user.id = :userId")
     List<Review> findAllByUserIdWithMovie(Long userId);
+
+    // 특정 영화 ID에 해당하는 리뷰 목록 조회. N+1 방지 (JOIN FETCH)
+    @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.movie.id = :movieId")
+    List<Review> findAllByMovieIdWithUser(Long movieId);
 }
