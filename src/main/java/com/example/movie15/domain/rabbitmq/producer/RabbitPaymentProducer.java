8e4e7d1be7c5ca9fae3f7018975a17c457685ec6
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-09 10:04:27 +0900

    Feat: RabbitMQ 프로듀서 코드 추가
    영화결제완료 메시지전송 메소드 추가
    영화취소완료 메시지전송 메소드 추가

---
+++ src/main/java/com/example/movie15/domain/rabbitmq/producer/RabbitPaymentProducer.java
@@ -0,0 +1,36 @@
+package com.example.movie15.domain.rabbitmq.producer;
+
+import com.example.movie15.domain.email.model.EmailMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RabbitPaymentProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    // 결제가 완료된 경우 메시지 전송. 결제완료시점에 이 메소드 호출해주면됨.
+    public void sendChargeEvent(String userEmail) {
+        EmailMessage emailMessage = new EmailMessage(
+                userEmail,
+                "영화결제가 완료되었습니다.",
+                "영화결제가 완료되었습니다! 시간에 맞춰 입장해주세요!"
+        );
+
+        // 메시지를 "chargeQueue"로 전송
+        rabbitTemplate.convertAndSend("chargeQueue", emailMessage);
+    }
+
+    // 결제가 취소된 경우 메시지 전송. 결제취소시점에 이 메소드 호출해주면됨.
+    public void sendCancelEvent(String userEmail) {
+        EmailMessage emailMessage = new EmailMessage(userEmail,
+                "영화결제가 취소되었습니다.",
+                "결제취소가 완료되었습니다!"
+        );
+
+        // 메시지를 "cancelQueue"로 전송
+        rabbitTemplate.convertAndSend("cancelQueue", emailMessage);
+    }
+}
