ef257797c28b4e8bd95047a601f673627371f38c
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-10 12:40:19 +0900

    Feat: RabbitMQ 기초구현
    현재 관련 코드들 주석처리 되어있음
    주석 처리 없이 실행시 Docker에서 RabbitMQ를 계속 실행시켜줘야해서 일단 관련코드 주석처리

--- src/main/java/com/example/movie15/global/config/RabbitConfig.java
+++ src/main/java/com/example/movie15/global/config/RabbitConfig.java
@@ -9,37 +9,37 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-
-    // 이메일 발송을 위한 큐 설정
-    @Bean
-    public Queue emailQueue() {
-        return new Queue("emailQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
-    }
-
-    // 영화결제이메일 발송을 위한 큐 설정
-    @Bean
-    public Queue chargeQueue() {
-        return new Queue("chargeQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
-    }
-
-    // 영화결제취소이메일 발송을 위한 큐 설정
-    @Bean
-    public Queue cancelQueue() {
-        return new Queue("cancelQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
-    }
-
-    // RabbitMQ와 연결할 ConnectionFactory 설정
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost"); // RabbitMQ 호스트
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        return connectionFactory;
-    }
-
-    // RabbitTemplate 설정
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
-    }
+//
+//    // 이메일 발송을 위한 큐 설정
+//    @Bean
+//    public Queue emailQueue() {
+//        return new Queue("emailQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
+//    }
+//
+//    // 영화결제이메일 발송을 위한 큐 설정
+//    @Bean
+//    public Queue chargeQueue() {
+//        return new Queue("chargeQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
+//    }
+//
+//    // 영화결제취소이메일 발송을 위한 큐 설정
+//    @Bean
+//    public Queue cancelQueue() {
+//        return new Queue("cancelQueue", true);  // true: 큐가 durable, 서버 재시작 후에도 큐가 유지됨
+//    }
+//
+//    // RabbitMQ와 연결할 ConnectionFactory 설정
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost"); // RabbitMQ 호스트
+//        connectionFactory.setUsername("guest");
+//        connectionFactory.setPassword("guest");
+//        return connectionFactory;
+//    }
+//
+//    // RabbitTemplate 설정
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        return new RabbitTemplate(connectionFactory);
+//    }
 }
