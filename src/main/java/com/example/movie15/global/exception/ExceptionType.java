c4d2b340a8d1362c2e5f2bdaf992fc7bd96d3501
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-11 15:30:44 +0900

    Feat: 리뷰작성시 유저는 하나의 영화에 하나의 댓글만 달수있게 코드추가
    리뷰관련 글로벌ExceptionType 추가

--- src/main/java/com/example/movie15/global/exception/ExceptionType.java
+++ src/main/java/com/example/movie15/global/exception/ExceptionType.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ExceptionType {
+    SAME_REVIEW(HttpStatus.NOT_FOUND,  "이미 작성된 리뷰와 같습니다. 수정을 원하면 다시 입력해주세요."),
+    ALREADY_REVIEW(HttpStatus.NOT_FOUND,  "이미 해당영화에 리뷰를 작성했습니다."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND,  "리뷰 정보를 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,  "해당 유저의 정보를 찾을 수 없습니다."),
     EXIST_USER(HttpStatus.BAD_REQUEST, "동일한 이메일의 사용자가 존재합니다."),
