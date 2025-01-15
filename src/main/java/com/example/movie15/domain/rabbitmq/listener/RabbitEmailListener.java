98247a64836d6a482aa65175d22102fb2183c0cd
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-15 21:26:05 +0900

    Feat: RabbitMQ 기능추가
    메시지의 만료시간 설정
    Redis 기능을 이용해서 결제취소시 큐에 들어가있던 지연메시지가 발송되지 않도록 기능추가

--- src/main/java/com/example/movie15/domain/rabbitmq/listener/RabbitEmailListener.java
+++ src/main/java/com/example/movie15/domain/rabbitmq/listener/RabbitEmailListener.java
@@ -1,20 +1,22 @@
 package com.example.movie15.domain.rabbitmq.listener;
 
 import com.example.movie15.domain.email.model.EmailMessage;
+import com.example.movie15.domain.rabbitmq.common.RedisKey;
 import com.example.movie15.domain.rabbitmq.service.RabbitEmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class RabbitEmailListener {
+public class RabbitEmailListener  {
 
     private final RabbitEmailService emailService;
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     /**
      * Delayed Queue 에서 메시지를 처리
@@ -50,11 +52,23 @@ public class RabbitEmailListener {
      */
     @RabbitListener(queues = "cancelQueue")
     public void cancelEmailMessage(EmailMessage emailMessage) {
-        try {
-            log.info("결제 취소 이메일 메시지를 수신했습니다. 유저: {}", emailMessage.getUserEmail());
-            emailService.sendEmail(emailMessage.getUserEmail(), emailMessage.getSubject(), emailMessage.getText());
-        } catch (Exception e) {
-            log.error("유저 '{}'의 결제 취소 이메일 메시지 처리에 실패했습니다.", emailMessage.getUserEmail(), e);
+
+        // Redis 에서 EmailMessage 를 가져옴
+        EmailMessage redisEmailMessage = (EmailMessage) redisTemplate.opsForHash().get(RedisKey.REMINDER_KEY, emailMessage.getBookingId());
+
+        // EmailMessage 가 null 이 아니면 이메일 그대로 발송.
+        // null 이면 발송하지 않고 취소된 메시지라는 로그만 남김.
+        if (redisEmailMessage != null) {
+            try {
+                log.info("결제 취소 이메일 메시지를 수신했습니다. 유저: {}", emailMessage.getUserEmail());
+                emailService.sendEmail(emailMessage.getUserEmail(), emailMessage.getSubject(), emailMessage.getText());
+            } catch (Exception e) {
+                log.error("유저 '{}'의 결제 취소 이메일 메시지 처리에 실패했습니다.", emailMessage.getUserEmail(), e);
+            }
+        }
+        else {
+            log.info("이미 결제취소한 메시지입니다. 이메일을 보내지 않습니다. 유저: {}", emailMessage.getUserEmail());
         }
     }
+
 }
--- src/main/java/com/example/movie15/domain/rabbitmq/producer/RabbitPaymentProducer.java
+++ src/main/java/com/example/movie15/domain/rabbitmq/producer/RabbitPaymentProducer.java
@@ -1,12 +1,16 @@
 package com.example.movie15.domain.rabbitmq.producer;
 
+import com.example.movie15.domain.booking.entity.Booking;
 import com.example.movie15.domain.email.model.EmailMessage;
+import com.example.movie15.domain.rabbitmq.common.RedisKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
 @Component
@@ -15,22 +19,34 @@ import java.time.temporal.ChronoUnit;
 public class RabbitPaymentProducer {
 
     private final RabbitTemplate rabbitTemplate;
-    //private final MovieReservationRepository reservationRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final long TEN_MINUTE = 600000;
 
     /**
      * 결제 완료 시 메시지를 큐에 전송
-     * @param userEmail 유저 이메일
+     *
+     * @param booking Booking 객체
      */
-    public void sendChargeEvent(String userEmail) {
+    public void sendChargeEvent(Booking booking) {
+
+        Long bookingId = booking.getId();
+
+        String userEmail = booking.getUser().getEmail();
+
         try {
             EmailMessage emailMessage = new EmailMessage(
+                    bookingId,
                     userEmail,
                     "영화결제가 완료되었습니다.",
                     "영화결제가 완료되었습니다! 시간에 맞춰 입장해주세요!"
             );
 
             log.info("유저 '{}'에게 'chargeQueue' 로 결제 이메일 메시지를 전송합니다.", userEmail);
-            rabbitTemplate.convertAndSend("chargeQueue", emailMessage);
+            rabbitTemplate.convertAndSend("chargeQueue", emailMessage, message -> {
+                message.getMessageProperties().setExpiration(String.valueOf(TEN_MINUTE)); // 10분안에 사용 안되면 삭제
+                return message;
+            });
+
         } catch (Exception e) {
             log.error("유저 '{}'에 대한 결제 이메일 메시지 전송에 실패했습니다.", userEmail, e);
         }
@@ -38,63 +54,103 @@ public class RabbitPaymentProducer {
 
     /**
      * 결제 취소 시 메시지를 큐에 전송
-     * @param userEmail 유저 이메일
+     *
+     * @param booking Booking 객체
      */
-    public void sendCancelEvent(String userEmail) {
+    public void sendCancelEvent(Booking booking) {
+
+        String userEmail = booking.getUser().getEmail();
+        Long bookingId = booking.getId();
+
         try {
+            Long deleteRedis = redisTemplate.opsForHash().delete(RedisKey.REMINDER_KEY, bookingId); // redis 에서 삭제
+
+            if (deleteRedis > 0) {
+                log.info("예약 메시지가 Redis 에서 성공적으로 취소되었습니다. (예약 ID: {})", bookingId);
+            } else {
+                log.info("Redis 에서 취소할 메시지를 찾을 수 없습니다. (예약 ID: {})", bookingId);
+            }
+
             EmailMessage emailMessage = new EmailMessage(
+                    bookingId,
                     userEmail,
                     "영화결제가 취소되었습니다.",
                     "결제취소가 완료되었습니다!"
             );
 
             log.info("유저 '{}'에게 'cancelQueue' 로 결제 취소 이메일 메시지를 전송합니다.", userEmail);
-            rabbitTemplate.convertAndSend("cancelQueue", emailMessage);
+            rabbitTemplate.convertAndSend("cancelQueue", emailMessage, message -> {
+                message.getMessageProperties().setExpiration(String.valueOf(TEN_MINUTE)); // 10분안에 사용 안되면 삭제
+                return message;
+            });
+
         } catch (Exception e) {
             log.error("유저 '{}'에 대한 결제 취소 이메일 메시지 전송에 실패했습니다.", userEmail, e);
         }
     }
 
-//    // TODO : 개선필요한 메소드
-//    /**
-//     * 예매시점에 "영화시작 30분 전 이메일 예약."
-//     * @param reservation 영화 예약 정보
-//     */
-//    public void movieStartReminderEmail(MovieReservation reservation) {
-//        try {
-//            // 영화 시작 시간 가져오기
-//            LocalDateTime movieStartTime = reservation.getMovieTime();
-//
-//            // 현재 시간과 비교해 딜레이 계산 (ms 단위)
-//            long delay = ChronoUnit.MILLIS.between(LocalDateTime.now(), movieStartTime.minusMinutes(30));
-//
-//            // 만약 영화 시작이 이미 30분 이내면 경고 로그 남김
-//            if (delay < 0) {
-//                log.warn("예약된 영화 시작 시간이 이미 30분 이내이거나 지났습니다. 이메일 예약이 불가능합니다. (영화 시작 시간: {}, 예약자: {})",
-//                        movieStartTime, reservation.getUserEmail());
-//                return;
-//            }
-//
-//            // 이메일 메시지 생성
-//            String subject = "곧 영화가 시작합니다!";
-//            String text = String.format("예매하신 영화가 30분 후에 시작됩니다. 입장을 준비해주세요! 영화 시간: %s", movieStartTime);
-//            EmailMessage emailMessage = new EmailMessage(reservation.getUserEmail(), subject, text);
-//
-//            // Delayed Exchange 를 통해 메시지 예약
-//            rabbitTemplate.convertAndSend(
-//                    "delayedExchange", // Delayed Exchange 이름
-//                    "emailKey",        // Routing Key
-//                    emailMessage,
-//                    message -> {
-//                        message.getMessageProperties().setDelay((int) delay); // 지연 시간 설정 (ms 단위)
-//                        return message;
-//                    }
-//            );
-//
-//            log.info("영화 시작 30분 전에 이메일 발송을 예약했습니다. (영화 시작 시간: {}, 예약자: {})",
-//                    movieStartTime, reservation.getUserEmail());
-//        } catch (Exception e) {
-//            log.error("영화 시작 알림 이메일 예약에 실패했습니다. (예약자: {})", reservation.getUserEmail(), e);
-//        }
-//    }
+    /**
+     * 예매시점에 "영화시작 30분 전 이메일 예약."
+     *
+     * @param booking 영화 예약 정보
+     */
+    public void movieStartReminderEmail(Booking booking) {
+
+        // 유저 이메일 가져오기
+        String userEmail = booking.getUser().getEmail();
+        // Booking Id 가져오기
+        Long bookingId = booking.getId();
+
+        try {
+            // 영화 시작 시간 가져오기 (LocalTime)
+            LocalTime movieStartTime = booking.getRunTime().getStartTime();
+
+            // 현재 날짜와 결합하여 LocalDateTime 으로 변환 (영화 시작 시간)
+            LocalDateTime movieStartDateTime = LocalDate.now().atTime(movieStartTime);
+
+            // 현재 시간과 비교해 딜레이 계산 (ms 단위)
+            long delay = ChronoUnit.MILLIS.between(LocalDateTime.now(), movieStartDateTime.minusMinutes(30));
+            // 메시지 만료시간
+            long expirationTime = ChronoUnit.MILLIS.between(LocalDateTime.now(), movieStartDateTime.plusMinutes(10));
+
+            // 만약 영화 시작이 이미 30분 이내면 경고 로그 남김
+            if (delay < 0) {
+                log.warn("예약된 영화 시작 시간이 이미 30분 이내이거나 지났습니다. 이메일 예약이 불가능합니다. (영화 시작 시간: {}, 예약자: {})",
+                        movieStartTime, userEmail);
+                return;
+            }
+
+            // 이메일 메시지 생성
+            String subject = "곧 영화가 시작합니다!";
+            String text = String.format("예매하신 영화가 30분 후에 시작됩니다. 입장을 준비해주세요! 영화 시간: %s", movieStartTime);
+            EmailMessage emailMessage = new EmailMessage(bookingId, userEmail, subject, text);
+
+            // bookingId 세팅
+            emailMessage.setBookingId(bookingId);
+
+            // Redis 에 저장
+            redisTemplate.opsForHash().put(RedisKey.REMINDER_KEY, bookingId, emailMessage); // 메인키 , 식별값 , 데이터
+
+            rabbitTemplate.convertAndSend(
+                    "delayedExchange", // Delayed Exchange 이름
+                    "emailKey",        // Routing Key
+                    emailMessage,
+                    message -> {
+                        // 지연 시간 설정 (ms 단위)
+                        message.getMessageProperties().setDelayLong(delay);
+
+                        // 만료 시간 설정 (예: 10분 후에 만료)
+                        message.getMessageProperties().setExpiration(String.valueOf(expirationTime)); // 만료시간 설정
+
+                        return message;
+                    }
+            );
+
+            log.info("영화 시작 30분 전에 이메일 발송을 예약했습니다. (영화 시작 시간: {}, 예약자: {})",
+                    movieStartTime, userEmail);
+        } catch (Exception e) {
+            log.error("영화 시작 알림 이메일 예약에 실패했습니다. (예약자: {})", userEmail, e);
+        }
+    }
+
 }
