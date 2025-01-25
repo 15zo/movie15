628dbc4e3652c4be9e17f6aacd565d45316c4b38
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2025-01-25 21:26:31 +0900

    Feat: RabbitMQ 수동 Ack 코드 추가

--- src/main/java/com/example/movie15/domain/rabbitmq/listener/RabbitUserSignupListener.java
+++ src/main/java/com/example/movie15/domain/rabbitmq/listener/RabbitUserSignupListener.java
@@ -5,7 +5,6 @@ import com.example.movie15.domain.user.entity.Role;
 import com.example.movie15.domain.user.entity.User;
 import com.example.movie15.domain.user.repository.UserRepository;
 import com.rabbitmq.client.Channel;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -22,7 +21,7 @@ public class RabbitUserSignupListener {
 
     private final UserRepository userRepository;
 
-    @RabbitListener(queues = QueueBindings.USER_SIGNUP_QUEUE)
+    @RabbitListener(queues = QueueBindings.USER_SIGNUP_QUEUE, ackMode = "MANUAL")
     public void deleteUser(Long userId, Channel channel, Message message) throws Exception {
         try {
             User user = userRepository.findByIdOrElseThrow(userId);
@@ -30,6 +29,7 @@ public class RabbitUserSignupListener {
             if (!user.isVerified() && user.getTokenExpiryTime().isBefore(LocalDateTime.now()) && user.getRole().equals(Role.USER)) {
                 userRepository.delete(user);
                 log.info("미인증 유저 삭제 완료: 이메일 : {}", user.getEmail());
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
             }
         } catch (Exception e) {
             log.warn("유저를 찾을 수 없습니다. userId: {}", userId);
@@ -37,7 +37,7 @@ public class RabbitUserSignupListener {
         }
     }
 
-    @RabbitListener(queues = QueueBindings.GLOBAL_DLQ)
+    @RabbitListener(queues = QueueBindings.GLOBAL_DLQ, ackMode = "MANUAL")
     public void processDeadLetterQueue(Long userId, Channel channel, Message message) throws IOException {
         log.warn("RabbitMQ : 처리 실패한 메시지 발견 : {}", userId);
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
