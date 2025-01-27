28f027ba735c9791a90be6f4c179f20c0060d7b8
fbqudrlf09 <fbqudrlf09@naver.com>
2025-01-27 09:04:52 +0900

    Feat: Redis 설정값 추가

--- src/main/java/com/example/movie15/global/config/RedisConfig.java
+++ src/main/java/com/example/movie15/global/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.example.movie15.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +13,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.password}")
+    private String password;
+    private static final String REDISSON_PREFIX = "redis://";
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -16,5 +28,14 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress(REDISSON_PREFIX + host + ":" + port)
+            .setPassword(password);
+        return Redisson.create(config);
     }
 }
