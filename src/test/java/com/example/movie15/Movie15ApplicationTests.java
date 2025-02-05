9ba79932fa0f7019c36f83d10ea7ee12e04ca778
diqkraud <daesan12>
2025-02-05 15:41:21 +0900

    테스트코드 주석 및 통합테스트주석

--- src/test/java/com/example/movie15/Movie15ApplicationTests.java
+++ src/test/java/com/example/movie15/Movie15ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.movie15;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class Movie15ApplicationTests {
 
 	@Test
