f2c5d80301711b2f839c5d2c74c1a6e6c2f40af3
diqkraud <daesan12>
2025-02-07 21:43:21 +0900

    Changes by user

--- src/main/java/com/example/movie15/global/config/SchedulerConfig.java
+++ src/main/java/com/example/movie15/global/config/SchedulerConfig.java
@@ -7,18 +7,21 @@ import org.springframework.scheduling.annotation.Scheduled;
 import com.example.movie15.domain.movie.service.MovieService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
+@Slf4j
 public class SchedulerConfig {
 
 private final MovieService movieService;
 
 	//fixedDelay = 10000 테스트용 10초마다 한 번 씩
 	//@Scheduled(cron="0 55 03 * * ?")
-	@Scheduled(cron="0 35 21 * * ?")
+	@Scheduled(cron="0 50 21 * * ?", zone = "Asia/Seoul")
 	public void updatePopularMoviesJob() throws Exception {
+		log.info("scheduler start");
 		movieService.savePopularMoviesToDatabase();
 		movieService.softDeleteOldMovies();
 	}
